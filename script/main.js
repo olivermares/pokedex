43c1396518e2fe017556af803bd835e47989f5d9
olivermares <olivermares@hotmail.com>
2023-07-18 16:49:26 +0200

    se ha añadido un filtro

--- script/main.js
+++ script/main.js
@@ -1,54 +1,96 @@
-//#region Peticion
+//#region Peticion,mapeo y dibujar
 
-
-const getCharacters = async () => {
-  const pokemon = [];
-  for (let i = 0; i < 150; i++) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`);
-    const res = await response.json();
-    pokemon.push(res);
-  }
+const getCharacters = async (url) => {
+  const imgDefault =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10263.png";
+  const response = await fetch(url);
+  const characters = await response.json();
+  const pokemon = {
+    name: characters.name,
+    img:
+      characters.sprites.front_default === null
+        ? imgDefault
+        : characters.sprites.front_default,
+  };
   return pokemon;
+};
+
+const getCharactersRecursive = async (url) => {
+  const result = [];
+  const response = await fetch(url);
+  const characters = await response.json();
+  characters.results.forEach(async (element) => {
+    result.push(await getCharacters(element.url));
+  });
+  aux =
+    characters.next === null
+      ? result
+      : await getCharactersRecursive(characters.next);
+  aux.forEach(async (element) => {
+    result.push(element);
+  });
+  return result;
 };
 
 const mappedCharacters = (characters) => {
   return characters.map((character) => ({
     name: character.name,
     image: character.sprites["front_default"],
-    type: character.types.map((type) => type.type.name).join(', '),
+    type: character.types.map((type) => type.type.name).join(", "),
     id: character.id,
   }));
 };
 
 const drawCharacters = (characters) => {
-  const main$$ = document.querySelector(".main")
-  //main$$.innerHTML = "";
+  const main$$ = document.querySelector('main');
+  main$$.innerHTML = "";
   for (const character of characters) {
-    let characterFigure$$ = document.createElement("figure");
-    characterFigure$$.setAttribute("class", "main-figure");
-    characterFigure$$.innerHTML = `
-      <div class="main-figure-container">
-        <h2>${character.name}</h2>
-        <img src="${character.image}" alt="${character.name}">
-      </div>
-  `;
-    main$$.appendChild(characterFigure$$);
-    //console.log(character.image)
+    let figure$$ = document.createElement("figure");
+    figure$$.className = "main-figure";
+    figure$$.innerHTML = `
+          <h3>${character.name}</h3>
+          <img src=${character.img} alt=${character.name}>
+      `;
+    //const btn$$ = document.createElement("button");
+    //btn$$.textContent = "BORRAR";
+    //btn$$.addEventListener("click", () => figure$$.remove());
+
+    //figure$$.appendChild(btn$$);
+    main$$.appendChild(figure$$);
   }
 };
+
+//#endregion
+
+//#region Filtro
+
+const drawHeader = (characters) =>{
+  const versions = [-1,150, 550,characters.length-1];
+  const header$$ = document.querySelector("header");
+  for (let index = 0; index < 3; index++) {
+    const btn$$ = document.createElement("button");
+    btn$$.textContent = `MOSTRAR V${index+1}`;
+
+    btn$$.addEventListener("click", () => {
+      drawCharacters(characters.slice(versions[index]+1, versions[index+1]));
+    });
+
+    header$$.appendChild(btn$$);
+  }
+}
 
 //#endregion
 
 //#region  init
 /*
 Funcion inicial donde ponemos todas las llamadas a funciones.
-Quedan dudas:
 */
 
 const init = async () => {
-  const characters =  await getCharacters();
-  const mappeCharacter = mappedCharacters(characters);
-  drawCharacters(mappeCharacter);
+  const url = `https://pokeapi.co/api/v2/pokemon`;
+  const characters = await getCharactersRecursive(url);  
+  drawCharacters(characters.slice(0, 150));
+  drawHeader(characters);
 };
 
 init();
